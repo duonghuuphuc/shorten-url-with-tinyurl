dec9186100ae962c22ab3c8aaac93caa2cc4f616
Duong Huu Phuc <huuphucduong@gmail.com>
2022-09-22 16:54:55 +0700

    First commit
    This first commit consists of the README, the source code, and the two demo TXT files

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# shorten-url-with-tinyurl
- Shorten a URL or a list of URLs with TinyURL service
+# Shorten a bulk list of URLs with TinyURL
+
+This repository presents a simple Python program that shortens a URL or a list of URLs by employing [TinyURL](https://tinyurl.com/) service. Before running this program, you need to obtain an API token from TinyURL at [here](https://tinyurl.com/app/settings/api). You should note that TinyURL is a paid service, so if you are using a Free plan, you will have some limitations such as 600 URLs/month when using API.
+
+## How to run this program
+First, you need to obtain an API token from TinyURL and insert it to line `34` in the `main.py`. Then, you have two options to run the program:
+
+ - To shorten a URL, execute `$ python main.py -u "LONG_URL"`
+ - To shorten a list of URLs, you need to insert those URLs to the `urls.txt`, and just execute the `main.py` without `-u` argument, i.e., `$ python main.py`
+
+*Note:* when shortening a list of URLs, the results will be placed in the `short_urls.txt` where the short URLs together with their original ones are separated by a `\t` character.
+
+## Links
+
+ - Obtain API token - [https://tinyurl.com/app/settings/api](https://tinyurl.com/app/settings/api)
+ - TinyURL OpenAPI - [https://tinyurl.com/app/dev](https://tinyurl.com/app/dev)
+
---
+++ main.py
@@ -0,0 +1,65 @@
+import http.client
+import json
+import string
+import random
+import time
+import argparse
+
+
+def generate_random_alias(length=6):
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+
+def generate_short_url(url, api_token=None, tinyurl_domain="tiny.one"):
+    if api_token is not None and url is not None:
+        conn = http.client.HTTPSConnection("api.tinyurl.com")
+        headers = {
+            'Authorization': 'Bearer ' + api_token,
+            'Content-Type': 'application/json'
+        }
+        payload = json.dumps({
+            "url": str(url),
+            "domain": str(tinyurl_domain),
+            "alias": "dhp-" + str(generate_random_alias())
+        })
+        conn.request("POST", "/create", payload, headers)
+        res = conn.getresponse()
+        data = json.loads(res.read().decode("utf-8"))
+        short_url = data.get('data').get('tiny_url')
+        return str(short_url)
+
+
+def main():
+    # configuration
+    api_token = 'INSERT_API_TOKEN_HERE'
+    tinyurl_domain = "tiny.one"  # tinyurl.com, tiny.one, rotf.lol
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "-url", help="Place a correct URL between a pair of quotation marks")
+    args = parser.parse_args()
+
+    # Case 1: short one URL only
+    if args.u is not None:
+        print(generate_short_url(url=args.u, api_token=api_token, tinyurl_domain=tinyurl_domain))
+    else:
+        # Case 2: short a list of urls
+        # Step 2.1: reading a list of long urls from text file
+        file = open('urls.txt', 'r', encoding='latin1').readlines()
+        urls = []
+        for u in file:
+            urls.append(u.replace('\n', '').replace('\t', ''))
+
+        # Step 2.2: create short URLs
+        f = open('short_urls.txt', 'w', encoding='utf-8')
+
+        print("Shorting URLs ...\n")
+        for url in urls:
+            short_url = generate_short_url(url=url, api_token=api_token, tinyurl_domain=tinyurl_domain)
+            f.write(short_url + '\t' + url + '\n')
+            time.sleep(.5)
+        print("... Done\n")
+        f.close()
+
+
+if __name__ == '__main__':
+    main()
